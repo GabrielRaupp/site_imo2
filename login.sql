001f6a9834301b122d3a8a4eb7f0e40c916f0a0e
Redes IFC <redes@lab32-pc01.sombrio.ifc.edu.br>
2024-03-01 11:51:41 -0300

    versão_4

--- login.sql
+++ login.sql
@@ -3,3 +3,11 @@ ID Int UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
 login Varchar(30),
 senha Varchar(40),
 Primary Key (ID)) ENGINE = MyISAM;
+
+Create table Produtos (
+Nome Varchar(50) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+Valor Int UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+Descricao Varchar(300),
+Primary Key (Nome) ,
+Primary Key (Valor)ENGINE = MyISAM,
+)
